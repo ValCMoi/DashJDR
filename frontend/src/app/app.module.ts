b823d0881f248a2d631bfa04c01557f88543eec6
ValCMoi <valentin.rouxel.pro@gmail.com>
2023-04-27 11:42:56 +0200

    ajout carte character

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RacePipe } from './race/race.pipe';
+import { CharacterCardComponent } from './character/character-card/character-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RacePipe
+    RacePipe,
+    CharacterCardComponent
   ],
   imports: [
     BrowserModule,
